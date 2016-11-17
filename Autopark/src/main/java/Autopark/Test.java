1f8aeea180cf4300088e5ca5f79c3e0bee177a71
Ihar Pryshchepau <Ihar Pryshchepau@epam.com>
2016-11-17 12:29:29 +0300

    - removed redundant prinln methods to Test class

--- Autopark/src/main/java/Autopark/Test.java
+++ Autopark/src/main/java/Autopark/Test.java
@@ -9,10 +9,8 @@ public class Test {
     public static void main(String args[]){
         Vehicle v1 = new Vehicle(1.0, 750, "Lada", 1989);
         Vehicle v2 = new Vehicle(1.1, 2500, "Mazda", 2002);
-        String info = v1.getInfo();
-        String info1 = v2.getInfo();
-        System.out.println (info);
-        System.out.println (info1);
+        System.out.println (v1.getInfo());
+        System.out.println (v2.getInfo());
 
     }
 }
